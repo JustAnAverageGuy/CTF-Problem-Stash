0dc09931c2dda6908b79080585103bfec46a0644
kn1-gh7 <goyalyuval15@gmail.com>
2024-01-22 21:43:35 +0530

    Updated server.js

--- OSINT/geosint/server.py
+++ OSINT/geosint/server.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/chall/chall_id')
+@app.route('/chall/<int:chall_id>')
 def view_chall(chall_id):
     try:
-        chall_url = challs["chall_id"][chall_id]["url"]
-        return render_template('chall.html', chall_id=chall_id, pan_url=chall_url)
+        chall_url = challs["chall_id"][str(chall_id)]["url"]
+        return render_template('chall.html', chall_id=str(chall_id), pan_url=chall_url)
     except:
         return render_template('index.html')
 
