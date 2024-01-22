93e582169ed7a6bde21cdcf09cdfdfa97ff14bf8
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-01-22 18:43:16 +0530

    Update crypto/do_you_know_RSA

--- crypto/do_you_know_RSA/script.py
+++ crypto/do_you_know_RSA/script.py
@@ -5,10 +5,10 @@ FLAG = b'COPS{fake_flag}'
 p,q = getPrime(1024), getPrime(1024)
 print(f'{p=}\n{q=}')
 
-m = bytes_to_long(FLAG)
+m = int((FLAG).hex(), 16)
 e = 0x10001
 n = p*q
 
 c = pow(m, e, n)
-print("c=",hex(c))
+print(f"c=0x{c:x}")
 
